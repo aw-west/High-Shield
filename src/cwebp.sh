12c77c7ce574d2f5a4c90197540331064c8efcab
aw-west <39385377+aw-west@users.noreply.github.com>
2023-05-10 19:25:45 +0100

    improved cwebp

--- src/cwebp.sh
+++ src/cwebp.sh
@@ -1,1 +1,2 @@
-find ./src/img/ | egrep '.jpeg|.jpg|.tiff|.tif|.png' | parallel --progress 'cwebp -quiet -af {} -o ./docs/www/{/.}.webp'
+find ./src/img/ -type d | sed 's/src\/img/docs\/www/g' | xargs mkdir -p
+find src/img | egrep '.jpeg|.jpg|.tiff|.tif|.png' | cut -d/ -f3- | parallel --eta 'cwebp -quiet -q 100 -af -resize 1200 0 ./src/img/{} -o ./docs/www/{.}.webp'
